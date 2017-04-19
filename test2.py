6bd72623f64ad9ddb78a969b43d6e9a25be8f8ab
indrah <indrhrph@gmail.com>
2017-04-19 10:31:51 -0400

    t

---
+++ test2.py
@@ -0,0 +1,20 @@
+import getopt,sys,os,re
+from collections import OrderedDict 
+import subprocess,re,pprint,csv
+
+backupdir='/var/tmp/pkgbck/'
+
+def exec_command(command):
+        print "execuing command " + command        
+        #active_link=subprocess.Popen(['dladm','show-phys'], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        active_link=subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        
+        lines=active_link.communicate()
+        print "Output " + lines[0]
+        print "Error " + lines[1]
+        
+cmd_list=['cp /etc/hosts $backupdir']
+for cmd in cmd_list:
+    exec_command(cmd)
+
+
