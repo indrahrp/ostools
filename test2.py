20ca8e5558115a69792fcbd2df236b4440d03d4c
indrah <indrhrph@gmail.com>
2017-04-26 21:28:51 -0400

    t

--- test2.py
+++ test2.py
@@ -13,10 +13,37 @@ def exec_command(command):
         print "Output " + lines[0]
         print "Error " + lines[1]
         
-cmd_list_backup=['cp /etc/hosts ' + backupdir,
-          'cp /etc/services ' + backupdir,
-           'netstat -ni > ' + backupdir + 'netstatni.txt',
-           'ipadm > ' + backupdir + 'ipadm.txt']
+cmd_list_backup=[
+            'mkdir -p ' + backupdir + 'usr/local',
+            'mkdir -p ' + backupdir + 'usr/pkg',
+            'mkdir -p ' + backupdir + 'var/db',
+            'mkdir -p ' + backupdir + 'var/bbmon',
+            'cd /usr/pkg; find . -depth -print | cpio -pdumv ' + backupdir + 'usr/pkg',   
+            'cd /usr/local;find . -depth -print | cpio -pdumv ' + backupdir + 'usr/local',
+            'cd /var/db;find . -depth -print | cpio -pdumv ' + backupdir + 'var/db',
+            'cd /var/bbmon; find . -depth -print | cpio -pdumv ' + backupdir + 'var/bbmon',
+            '/usr/pkg/sbin/pkg_info > ' + backupdir + 'pkginfoall.txt',
+            'tar cvf ' + backupdir + 'opt.tar /opt',
+            'format > ' + backupdir + 'format.txt 0</dev/null',
+            
+            'netstat -i > ' + backupdir + 'netstatii',
+            'netstat -nrv > ' + backupdir + 'netstatnrv',
+            'cp /etc/inet/static_routes-DefaultFixed  ' + backupdir + 'static_routes-DefaultFixed.txt',
+            'cp /etc/system ' + backupdir + 'system',
+            'cp /etc/rc2.d/S68ndd ' + backupdir,    
+            'cp /etc/resolv.conf ' + backupdir,
+            'cp /etc/nsswitch.conf ' + backupdir,
+            'cp  /etc/zephyr.servers ' + backupdir,
+            'ipadm > ' + backupdir + 'ipadm',
+            'dladm show-link > ' + backupdir + 'dladmshowlinks',
+            'dladm show-phys -L > ' + backupdir + 'dladmshowphysL',
+            'dladm show-vnic > ' + backupdir + 'dladmshowvnic',
+            'ifconfig -a > ' + backupdir + 'ifconfiga'
+            
+            'cp /etc/hosts ' + backupdir,
+            'cp /etc/services ' + backupdir,
+            'netstat -ni > ' + backupdir + 'netstatni.txt',
+            'ipadm > ' + backupdir + 'ipadm.txt']
 cmd_list_restore=['mkdir /etc/sysadmin && (mv /etc/inet/hosts /etc/inet/services /etc/sysadmin/;ln -s /etc/sysadmin/hosts /etc/inet/hosts;ln -s /etc/sysadmin/services /etc/inet/services)',
                  'cp /etc/ntp.keys /etc/' ]
 
