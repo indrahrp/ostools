5a34d336273f7e5017211828821ca5d910d7c40e
indrah <indrhrph@gmail.com>
2017-05-09 19:22:19 -0400

    perfecting

--- fixipadm2.py
+++ fixipadm2.py
@@ -6,12 +6,13 @@ import subprocess,re,pprint,csv
 
 def hextodec(shex):
 	netmask=[]
-	print "shex arg is " + shex
+	#print "shex arg is " + shex
 	for cnt in range(0,7,2):
-		print "cnt is " + str(cnt)
-		print 'shex is ' + shex[cnt:(cnt+2)]
+		#print "cnt is " + str(cnt)
+		#print 'shex is ' + shex[cnt:(cnt+2)]
+		
 		dec=int(shex[cnt:(cnt+2)],16)
-		print "dec is "+ str(dec)
+		#print "dec is "+ str(dec)
 		#netmask += "." + str(dec)
 		netmask.append(str(dec))
 		#print "netmask hextodec "+ netmask
@@ -25,26 +26,22 @@ def find_int(str1):
     (ixgbe\d+|igb\d+|e1000g\d+).*mtu\s+(\d+).*\n\s+inet\s+(\d+.\d+.\d+.\d+)\s+netmask\s+(\w{8}).*\n\s+ether\s+(\w{1,2}:\w{1,2}:\w{1,2}:\w{1,2}:\w{1,2}:\w{1,2})
     ''',re.IGNORECASE | re.VERBOSE)
 
-    #print "sting ins" + str1
     result=Regex.findall(str1)
     print "result " + str(result)
     if result:
         for res in result:
-        	#listtmp=[]
-            ###if res[3].startswith(args[0]):
-            print "ip found " + res[0] + res[1] + res[2] + " " + res[3] + " " + res[4]
+            #print "ip found " + res[0] + res[1] + res[2] + " " + res[3] + " " + res[4]
             listtmp=[]
             netmask=hextodec(res[3])
-            print "netmask is "+ netmask            
-            print "res2 is " + res[2]
-            #ipall='u' + "'"+ res[2]+'/'+netmask + "'"
-            ipall= unicode(res[2]+'/'+netmask)
-            
+            #print "netmask is "+ netmask            
+            #print "res2 is " + res[2]
+            ipall= unicode(res[2]+'/'+netmask)   
+       	    #important inforrmation
             print "ipall " + ipall
             my_ip = ipaddress.ip_interface(u'100.110.120.130')
-            print "my ip " + str(my_ip)
+            #print "my ip " + str(my_ip)
             my_ip = ipaddress.ip_interface(ipall)
-            print "my ip " + str(my_ip) + " network " + str(my_ip.network) + " broadcast " + str(my_ip.network.broadcast_address)            
+            #print "my ip " + str(my_ip) + " network " + str(my_ip.network) + " broadcast " + str(my_ip.network.broadcast_address)            
             listtmp=[res[0],res[1],res[2],res[3],res[4],netmask,str(my_ip.network),str(my_ip.network.broadcast_address)]
             intlist.append(listtmp)
             
@@ -71,7 +68,6 @@ def ipadm_setip(intlist):
 		print "ipadm setting up  .."	
         
 		for int in	intlist:
-			#print "ipadm assign to pertama " + int[7]
 			print "ipadm assign lagi to " + int[9]
 			active_link=subprocess.Popen(['ipadm','create-ip',int[9]], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 			lines=active_link.communicate()
@@ -81,44 +77,10 @@ def ipadm_setip(intlist):
 			lines=active_link.communicate()
 			print "lines 2 errro is " + lines[1]		
 		
-def ReadSwitchConfigFromFile(Filename):
+def ReadFromFile(Filename):
 	readfile=open(Filename,'r')
 	result=readfile.read()
 	return result
-
-ifconfiga=ReadSwitchConfigFromFile('ifconfiga')
-print "ifconfiga " + ifconfiga
-
-svrname='bunkerx1'
-domainname='tdn.pln.ilx.com'
-hostipdict={}
-
-hostfile=ReadSwitchConfigFromFile('hosts')
-##print "hostfile is : " + hostfile
-for entries in hostfile.splitlines():
-	#print "entry1 " + entry
-	hostiplist=[]
-	if svrname in entries:
-		print "entry " + str(entries)
-		ent=entries.split()
-		#hostiplist=[ent[1]]		
-		hostipdict[str(ent[0])]=ent[1]
-		print "\n\n"
-			
-print "hostipdict "+  str(hostipdict)
-
-intl=find_int(ifconfiga)
-for int in intl:
-	netname=str(hostipdict[int[2]]).replace('.'+ domainname,"").replace(svrname + '.' ,"")
-	#print "int is " + str(int) + ' with network name ' + netname
-	int.append(netname)
-	print "int dengan netame " + str(int)
-
-phys=OrderedDict()
-
-dladm_showphys(phys)
-print "phys is " + str(phys)
-
 
 
 def inttoconfigure():
@@ -129,18 +91,14 @@ def inttoconfigure():
 		search_dev=int[0]
 		for netname, values in phys.items():
 			if values['device'] == search_dev:
-				print "netname for " +  " search_dev is " + netname
-			
+				print "netname for " +  " search_dev is " + netname			
 				int.append(netname)
 				nintlist.append(int)
 				break
-				#print values['device']
+			
 	print "after all " + str(nintlist)
 	return nintlist
 
-intforconfig=inttoconfigure()
-print "intforconfig is " + str(intforconfig)
-ipadm_setip(intforconfig)
 
 
 def ping_server(serverIP,broadcast,linux):
@@ -168,7 +126,6 @@ def pingbroadint(toverify):
 		ping_server(ip[7],True,False)
 	
 	
-pingbroadint(intforconfig)	
 
 def usage():
     print "xxxx "
@@ -176,10 +133,55 @@ def usage():
 #def validate():
 
 
+def gatherinfo(svrname):
+	ifconfiga=ReadFromFile('ifconfiga')
+	print "ifconfiga " + ifconfiga
+
+	#svrname=svrname
+	domainname='tdn.pln.ilx.com'
+	hostipdict={}
+
+	hostfile=ReadFromFile('hosts')
+	for entries in hostfile.splitlines():
+		hostiplist=[]
+		if svrname in entries:
+			#print "entry " + str(entries)
+			ent=entries.split()
+			hostipdict[str(ent[0])]=ent[1]
+			print "\n\n"
+			
+	print "hostipdict "+  str(hostipdict)
+
+
+	intl=find_int(ifconfiga)
+	for int in intl:
+		netname=str(hostipdict[int[2]]).replace('.'+ domainname,"").replace(svrname + '.' ,"")
+	#print "int is " + str(int) + ' with network name ' + netname
+		int.append(netname)
+		print "int dengan netame " + str(int)
+
+	phys=OrderedDict()
+
+	dladm_showphys(phys)
+	print "phys is " + str(phys)
+
+
+	intforconfig=inttoconfigure()
+	print "intforconfig is " + str(intforconfig)
+
+
+
+#ipadm_setip(intforconfig)
+
+
+#gatherinfo(svrname)
+#pingbroadint(intforconfig)	
+
+
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hq")
+        opts, args = getopt.getopt(sys.argv[1:], "B:q")
     except getopt.GetoptError as err:
         print str(err) 
         usage()
@@ -188,8 +190,10 @@ def main():
         if o == "-h":
             usage()
             sys.exit(0)
-        if o == "-q":
-            print "test"
+        if o == "-B":
+            broadcastip=gatherinfo(a)
+    	    pingbroadint(broadcastip)
+            
     
 if __name__ == "__main__":
     main()
