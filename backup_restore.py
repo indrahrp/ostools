f843e14138cd25fd30d37ebedf05f1fbaf0f9a3b
indrah <indrhrph@gmail.com>
2017-05-12 22:55:50 -0400

    major

--- backup_restore.py
+++ backup_restore.py
@@ -123,6 +123,10 @@ cmd_list_backup=[
             '(cd  /usr/openwin/lib/app-defaults/ && cp -p * ' + backupdir + 'usr/openwin/lib/app-defaults/)',
             '(cd usr/lib/X11/app-defaults && cp -p * ' + backupdir + 'usr/lib/X11/app-defaults/)',
             'cp -p /usr/lib/libsybdb* ' + backupdir + 'autosys/' 
+            
+            
+            '(mkdir -p ' + backupdir + 'etcdir ; cd /etc/ && find . -depth -print |cpio -pdumv' + backupdir + 'etcdir)'
+          
            ]
 
 
